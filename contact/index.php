3d7b36e2ca28c4adf4246729c8e07f280859ad69
Asik Mydeen <ammydeen@amazon.com>
2019-03-01 19:59:52 +0530

    Check

--- contact/index.php
+++ contact/index.php
@@ -1,3 +1,56 @@
 <?php
-echo "Hello mail"
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    //header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Credentials: true');
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+}
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
+
+if (empty($_POST['fname']) && empty($_POST['email'])) die();
+
+if ($_POST)
+	{
+
+	// set response code - 200 OK
+
+	http_response_code(200);
+	$subject = $_POST['fname'];
+	$to = "ammydeen@amazon.com";
+	$from = $_POST['email'];
+
+	// data
+
+	$msg = $_POST['number'] . $_POST['message'];
+
+	// Headers
+
+	$headers = "MIME-Version: 1.0\r\n";
+	$headers.= "Content-type: text/html; charset=UTF-8\r\n";
+	$headers.= "From: <" . $from . ">";
+	mail($to, $subject, $msg, $headers);
+
+	// echo json_encode( $_POST );
+
+	echojson_encode(array(
+		"sent" => true
+	));
+	}
+  else
+	{
+
+	// tell the user about error
+
+	echojson_encode(["sent" => false, "message" => "Something went wrong"]);
+	}
+
 ?>
